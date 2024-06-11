68683a92022551c8d0e6d03e27eeda9bdb0a60e9
Viktor Talashko <viktortalashko@gmail.com>
2024-06-11 22:10:14 +0300

    make build

---
+++ index.26c8e3d8.js
@@ -0,0 +1,2 @@
+!function e(n,t){var r=document.createElement("ul");for(var a in t){var c=document.createElement("li");c.textContent=a,r.append(c),Object.keys(t[a]).length&&e(c,t[a])}n.append(r)}(document.querySelector("#tree"),{Drink:{Wine:{},Schnaps:{}},Fruit:{Red:{Cherry:{},Strawberry:{}},Yellow:{Banana:{},Pineapple:{}}}});
+//# sourceMappingURL=index.26c8e3d8.js.map
